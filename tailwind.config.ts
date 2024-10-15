30390204710f43def08996b75d02b374a5316fe5
raafy <raafyshiham@gmail.com>
2024-10-15 21:07:25 +0800

    style: theme update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,12 +9,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        jetbrains: ["var(--font-jetbrains-mono)"],
+        fira: ["var(--font-fira-code)"],
       },
     },
   },
   plugins: [],
 };
+
 export default config;
