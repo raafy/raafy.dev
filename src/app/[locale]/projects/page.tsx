dfc5f6005687ce7a1ef4526db7ccce13a9f3d5ee
raafy <raafyshiham@gmail.com>
2024-10-18 22:04:26 +0800

    refactor: projects page refactored

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,7 +1,27 @@
-export default function ProjectsPage() {
+import { useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export default function ProjectsPage({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  unstable_setRequestLocale(locale);
+  const t = useTranslations("ProjectsPage");
+
+  const data = t.raw("projects") as {
+    name: string;
+    technologies: string[];
+    platforms: string[];
+    description: string;
+    urls: { platform: string; url: string }[];
+  }[];
+
+  console.log();
+
   return (
     <div>
-      <p>Projects Page</p>
+      <p>{t("heading")}</p>
     </div>
   );
 }
