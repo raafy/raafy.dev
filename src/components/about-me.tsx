7ff356cdf52c51c5b6762763c12d4808b538dedf
raafy <raafyshiham@gmail.com>
2024-11-25 14:30:55 +0800

    removed unused import

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Metadata } from "next";
 import { useTranslations } from "next-intl";
 
 export function generateStaticMetadataMetadata() {}
